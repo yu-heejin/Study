e71787acdd209da097c09cb4d08c0cb7f09b8f83
yu-heejin <jkgt0521@naver.com>
2022-05-04 13:08:20 +0900

    commit

---
+++ MobileSoftware/week10/Practice02/app/src/main/java/ddwucom/moblie/week10/practice02/MainActivity.java
@@ -0,0 +1,14 @@
+package ddwucom.moblie.week10.practice02;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
