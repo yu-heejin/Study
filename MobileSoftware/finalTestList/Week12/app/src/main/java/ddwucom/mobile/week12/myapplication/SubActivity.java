a6558b3ee7baf130d3a7dc5f9ff21c4ec5469cbd
hee <jkgt0521@naver.com>
2022-06-13 04:47:48 +0900

    android: final test

---
+++ MobileSoftware/finalTestList/Week12/app/src/main/java/ddwucom/mobile/week12/myapplication/SubActivity.java
@@ -0,0 +1,13 @@
+package ddwucom.mobile.week12.myapplication;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SubActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sub);
+    }
+}
