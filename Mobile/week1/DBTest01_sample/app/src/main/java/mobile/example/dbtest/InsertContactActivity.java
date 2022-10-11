6ee2d7920420b59859aac4a7c7ca037d96c32e46
hee <jkgt0521@naver.com>
2022-10-12 04:13:15 +0900

    SQLite review

---
+++ Mobile/week1/DBTest01_sample/app/src/main/java/mobile/example/dbtest/InsertContactActivity.java
@@ -0,0 +1,38 @@
+package mobile.example.dbtest;
+
+import android.app.Activity;
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class InsertContactActivity extends Activity {
+
+	ContactDBHelper helper;
+	EditText etName;
+	EditText etPhone;
+	EditText etCategory;
+	
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_insert_contact);
+
+//      DBHelper 생성
+		helper = new ContactDBHelper(this);
+		
+		etName = (EditText)findViewById(R.id.editText1);
+		etPhone = (EditText)findViewById(R.id.editText2);
+		etCategory = (EditText)findViewById(R.id.editText3);
+	}
+	
+	
+	public void onClick(View v) {
+
+
+	}
+	
+
+}
