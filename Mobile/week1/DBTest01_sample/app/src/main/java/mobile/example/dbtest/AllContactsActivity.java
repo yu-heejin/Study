951be64180a126bfb3fbe44c9325fca09783a50d
hee <jkgt0521@naver.com>
2022-10-14 02:55:37 +0900

    [android] week01

--- Mobile/week1/DBTest01_sample/app/src/main/java/mobile/example/dbtest/AllContactsActivity.java
+++ Mobile/week1/DBTest01_sample/app/src/main/java/mobile/example/dbtest/AllContactsActivity.java
@@ -35,9 +35,30 @@ public class AllContactsActivity extends Activity {
 
 	@Override
 	protected void onResume() {
-		super.onResume();
-		
+		super.onResume();   // 화면이 감춰졌다가 호출될 때 다시 실행
+		//즉 화면을 나갔다가 다시 들어오면 화면에 같은 데이터가 중복으로 들어갈 가능성 O
 
+		contactList.clear();   //DB에 값을 넣기 전 기존 리스트 삭제 -> 데이터 중복 문제 해결
+		//새로 데이터를 읽어와도 전혀 문제되지 않음
+		
+		SQLiteDatabase db = helper.getReadableDatabase();
+		Cursor cursor = db.rawQuery("select * from " + ContactDBHelper.TABLE_NAME, null);
+
+		while(cursor.moveToNext()) {
+			ContactDto dto = new ContactDto();
+
+			dto.setId(cursor.getInt(cursor.getColumnIndex("_id")));
+			dto.setName(cursor.getString(cursor.getColumnIndex(ContactDBHelper.COL_NAME))); //cursor.getString(1)과 동일
+			dto.setPhone(cursor.getString(cursor.getColumnIndex(ContactDBHelper.COL_PHONE)));
+			dto.setCategory(cursor.getString(cursor.getColumnIndex(ContactDBHelper.COL_CAT)));
+
+			contactList.add(dto);
+		}
+
+		adapter.notifyDataSetChanged();  //화면 값 갱신 요청, 새로운 DB값으로 변경
+
+		cursor.close();
+		helper.close();
 	}
 
 }
--- Mobile/week1/DBTest01_sample/app/src/main/java/mobile/example/dbtest/InsertContactActivity.java
+++ Mobile/week1/DBTest01_sample/app/src/main/java/mobile/example/dbtest/InsertContactActivity.java
@@ -22,7 +22,8 @@ public class InsertContactActivity extends Activity {
 
 //      DBHelper 생성
 		helper = new ContactDBHelper(this);
-		
+
+		// 화면 요소를 가져옴
 		etName = (EditText)findViewById(R.id.editText1);
 		etPhone = (EditText)findViewById(R.id.editText2);
 		etCategory = (EditText)findViewById(R.id.editText3);
@@ -30,8 +31,26 @@ public class InsertContactActivity extends Activity {
 	
 	
 	public void onClick(View v) {
+		//helper에서 db를 가져옴
+		SQLiteDatabase db = helper.getWritableDatabase();
 
+		ContentValues row = new ContentValues();
+		row.put(ContactDBHelper.COL_NAME, etName.getText().toString());
+		row.put(ContactDBHelper.COL_PHONE, etPhone.getText().toString());
+		row.put(ContactDBHelper.COL_CAT, etCategory.getText().toString());
 
+		//insert문은 long타입의 값을 반환
+		//insert 수행 후 영향을 받은 row의 개수를 반환함! -> result값이 0보다 크면 정상적으로 반환이 됐음을 뜻함
+		long result = db.insert(ContactDBHelper.TABLE_NAME, null, row);
+
+		//SQL문 상에서는 ' '로 묶여야만 문자열로 취급됨
+		//exec의 경우 void타입 -> 반환값이 없기 때문에 해당 기능이 제대로 수행이 됐는지 알 수 없음 (try-catch문 사용 필요)
+		db.execSQL("insert into " + ContactDBHelper.TABLE_NAME + " values ( NULL, '"
+		+ etName.getText().toString() + "', '" + etPhone.getText().toString() + "', '" + etCategory.getText().toString() + "');");
+		//메소드 방식으로 사용하는 것이 더 편함
+
+		helper.close();    //명시적으로 커밋을 위해 사용한다
+		//db를 close하는 것이 아니라 helper를 close
 	}
 	
 
